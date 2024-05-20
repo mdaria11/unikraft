e4feb78d16270f627abfa17e7d8d6aeb0673351d
mdaria11 <daria.moldovan11@gmail.com>
2024-05-20 18:51:11 +0300

    drivers/ukbus/pci: Consistency changes to PCI macros
    Fixed some PCI fields macros to be extracted from their
    registers in a consistent manner and made some small coding
    style changes to the IRQ macros.

--- drivers/ukbus/pci/include/uk/bus/pci.h
+++ drivers/ukbus/pci/include/uk/bus/pci.h
@@ -210,16 +210,16 @@ static struct pci_bus_handler ph __unused;
 #define PCI_ENABLE_BIT              (1u << 31)
 
 #define PCI_CONF_CLASS_ID          (0x08)
-#define PCI_CONF_CLASS_ID_SHFT     (16)
-#define PCI_CONF_CLASS_ID_MASK     (0xFF00)
+#define PCI_CONF_CLASS_ID_SHFT     (24)
+#define PCI_CONF_CLASS_ID_MASK     (0xFF)
 
 #define PCI_CONF_VENDOR_ID          (0x00)
 #define PCI_CONF_VENDOR_ID_SHFT     (0)
-#define PCI_CONF_VENDOR_ID_MASK     (0x0000FFFF)
+#define PCI_CONF_VENDOR_ID_MASK     (0xFFFF)
 
 #define PCI_CONF_DEVICE_ID          (0x00)
 #define PCI_CONF_DEVICE_ID_SHFT     (16)
-#define PCI_CONF_DEVICE_ID_MASK     (0x0000FFFF)
+#define PCI_CONF_DEVICE_ID_MASK     (0xFFFF)
 
 #define PCI_CONF_SUBSYSVEN_ID          (0x2c)
 #define PCI_CONF_SUBSYSVEN_ID_SHFT     (0)
@@ -227,11 +227,11 @@ static struct pci_bus_handler ph __unused;
 
 #define PCI_CONF_SUBCLASS_ID          (0x08)
 #define PCI_CONF_SUBCLASS_ID_SHFT     (16)
-#define PCI_CONF_SUBCLASS_ID_MASK     (0x00FF)
+#define PCI_CONF_SUBCLASS_ID_MASK     (0xFF)
 
 #define PCI_CONF_SECONDARY_BUS          (0x18)
-#define PCI_CONF_SECONDARY_BUS_SHFT     (0)
-#define PCI_CONF_SECONDARY_BUS_MASK     (0xFF00)
+#define PCI_CONF_SECONDARY_BUS_SHFT     (8)
+#define PCI_CONF_SECONDARY_BUS_MASK     (0xFF)
 
 #define PCI_HEADER_TYPE_MSB_MASK   (0x80)
 #define PCI_CONF_HEADER_TYPE       (0x00)
@@ -242,9 +242,9 @@ static struct pci_bus_handler ph __unused;
 #define PCI_CONF_SUBSYS_ID_SHFT     (16)
 #define PCI_CONF_SUBSYS_ID_MASK     (0xFFFF)
 
-#define PCI_CONF_IRQ                (0X3C)
+#define PCI_CONF_IRQ                (0x3C)
 #define PCI_CONF_IRQ_SHFT           (0x0)
-#define PCI_CONF_IRQ_MASK           (0XFF)
+#define PCI_CONF_IRQ_MASK           (0xFF)
 
 #define PCI_CONF_IOBAR              (0x10)
 #define PCI_CONF_IOBAR_SHFT         (0x0)
